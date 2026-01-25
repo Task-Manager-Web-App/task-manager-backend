a8ce0772eba838d51e9756a5965a18028037c1ce
nuwan98765432 <t3sunu00@students.oamk.fi>
2026-01-26 01:37:20 +0200

    nuwan commit1

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -5,7 +5,33 @@ require('dotenv').config();
 const { supabase } = require('../supabase-client.js');
 
 
+// POST -login
+loginRouter.post('/', async (req, res) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password required' });
+  }
+
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return res.status(401).json({ message: error.message });
+    }
+
+    res.status(200).json({
+      message: 'Login successful',
+      user: data.user,
+      session: data.session,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', err });
+  }
+});
 
 
 
--- routes/register-routes.js
+++ routes/register-routes.js
@@ -4,8 +4,35 @@ const registerRouter = express.Router();
 require('dotenv').config();
 const { supabase } = require('../supabase-client.js');
 
+// POST /api/register
+registerRouter.post('/', async (req, res) => {
+  const { email, password, role } = req.body;
 
+  if (!email || !password || !role) {
+    return res.status(400).json({ message: 'All fields required' });
+  }
 
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: { role }, // stored in user_metadata
+      },
+    });
+
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    res.status(201).json({
+      message: 'User registered successfully',
+      user: data.user,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', err });
+  }
+});
 
 
 
